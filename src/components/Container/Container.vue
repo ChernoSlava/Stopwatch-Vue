8ac2be4d5494d6938e79d2f873a410e78086b5f8
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2023-03-27 14:57:22 +0300

    feat: create container for stopwatch

---
+++ src/components/Container/Container.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  color: white;
+  background-color: #696969;
+  width: 225px;
+  height: 120px;
+  border: 1px solid transparent;
+}
+</style>
