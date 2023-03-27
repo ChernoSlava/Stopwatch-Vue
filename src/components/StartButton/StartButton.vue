bc078e77acbfc81c5e369bb2411995fc8ed8a03b
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2023-03-27 16:40:06 +0300

    feat: create StartButton component

---
+++ src/components/StartButton/StartButton.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+defineProps({
+  onClick: Function,
+});
+</script>
+
+<template>
+      <div class="StartButton" :onClick="onClick">
+        <svg
+          width="17"
+          height="20"
+          viewBox="0 0 17 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M0 20V0L17 10L0 20Z" fill="#9E9E9E" />
+        </svg>
+      </div>
+</template>
+
+<style scoped>
+.StartButton {
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+}
+</style>
