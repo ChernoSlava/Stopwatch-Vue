121651700b60004b6fcffdb1e22e19830a0b4f8f
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2023-03-27 15:20:44 +0300

    feat: add styles in container

--- src/components/Container/Container.vue
+++ src/components/Container/Container.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+
+</script>
 
 <template>
   <div class="container">
@@ -7,11 +9,15 @@
 </template>
 
 <style scoped>
-.container {
-  color: white;
-  background-color: #696969;
-  width: 225px;
-  height: 120px;
-  border: 1px solid transparent;
-}
+  .container {
+    color: white;
+    background-color: #696969;
+    padding: 20px 0px;
+    width: 225px;
+    height: 120px;
+    border: 1px solid transparent;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 </style>
